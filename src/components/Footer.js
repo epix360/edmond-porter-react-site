fe4ef5717e079b45c1a29fc6ccf50f689e67e7f7
Eric Porter <porter.eric.e@gmail.com>
2026-04-27 16:58:48 -0600

    Add league of utah writers link to src file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,9 +11,10 @@ const Footer = () => {
                     <Link className="text-xl font-headline italic text-white mb-2 block" href="/">Edmond A Porter</Link>
                     <p className="text-slate-400 font-body text-sm tracking-wide">© {currentYear} Edmond A Porter. All rights reserved.</p>
                 </div>
+                <a href="https://www.leagueofutahwriters.com/" target="_blank" rel="noopener noreferrer">League of Utah Writers</a>
                 <div className="flex items-center space-x-4 opacity-80 hover:opacity-100 duration-300">
-                    <a className="p-2 bg-slate-800 rounded-full hover:bg-slate-700 transition-colors" href="https://www.goodreads.com/author/show/60996287.Edmond_A_Porter" target="_blank" rel="noopener noreferrer"><span className="material-symbols-outlined text-white">book_2</span></a>
-                    <a className="p-2 bg-slate-800 rounded-full hover:bg-slate-700 transition-colors" href="https://medium.com/feed/@eporter609" target="_blank" rel="noopener noreferrer"><span className="material-symbols-outlined text-white">rss_feed</span></a>
+                    <a className="p-2 bg-slate-800 rounded-full hover:bg-slate-700 transition-colors" title="Goodreads" href="https://www.goodreads.com/author/show/60996287.Edmond_A_Porter" target="_blank" rel="noopener noreferrer"><span className="material-symbols-outlined text-white">book_2</span></a>
+                    <a className="p-2 bg-slate-800 rounded-full hover:bg-slate-700 transition-colors" title="RSS Feed" href="https://medium.com/feed/@eporter609" target="_blank" rel="noopener noreferrer"><span className="material-symbols-outlined text-white">rss_feed</span></a>
                 </div>
             </div>
         </footer>
